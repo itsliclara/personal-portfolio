f0a1bc1e1c5925be8cae2fd7220e1cc79878d3ce
itsliclara <itsliclara@gmail.com>
2026-08-02 18:59:42 +0400

    y

--- script.js
+++ script.js
@@ -1,1 +1,52 @@
+gsap.registerPlugin(ScrollTrigger);
 
+const tl = gsap.timeline();
+
+tl.from(".navbar-wrapper",{
+    y:-80,
+    opacity:0,
+    duration:1
+})
+
+.from(".small-title",{
+    y:20,
+    opacity:0,
+    duration:.6
+},"-=.5")
+
+.from(".hero-title",{
+    y:60,
+    opacity:0,
+    duration:.9
+},"-=.4")
+
+.from(".hero-subtitle",{
+    y:40,
+    opacity:0,
+    duration:.7
+},"-=.5")
+
+.from(".hero-description",{
+    y:30,
+    opacity:0,
+    duration:.7
+},"-=.4")
+
+.from(".button-wrapper",{
+    y:20,
+    opacity:0,
+    duration:.6
+},"-=.4")
+
+.from(".hero-photo",{
+    x:120,
+    opacity:0,
+    scale:.9,
+    duration:1
+},"-=1")
+
+.from(".badge",{
+    y:25,
+    opacity:0,
+    duration:.6
+},"-=.5");
